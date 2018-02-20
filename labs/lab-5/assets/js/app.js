58515e98ad68a78ac3a1f6d970d17989709f76c4
Vidya Keshavan <vidyakeshavan91@gmail.com>
2018-02-20 09:08:21 -0500

    start lab 5

---
+++ labs/lab-5/assets/js/app.js
@@ -0,0 +1,24 @@
+$('.item').click(function() {
+  //is the item already in the cart?
+  //has the user already clicked this item?
+  
+  if (!$(this).hasClass('in-cart')){
+  
+  
+      //Add in-cart class to iterms that have been clicked
+      $(this).addClass('in-cart');
+  
+  
+      //increment the cart number
+      //find current value of .item-count
+      //add 1 to current .item-count
+      //represent new cart number in html on .item-count
+  
+      var current_count = $('.item-count'). html(),
+          current_value = Math.abs(current_count),
+          new_count     = current_value + 1;
+          
+      $('.item-count').html(new_count);
+  }
+  
+  });
