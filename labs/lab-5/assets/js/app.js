6b1ab27f2919efb3f85c25f73ec9670b59346f87
Vidya Keshavan <vidyakeshavan91@gmail.com>
2018-02-20 11:37:18 -0500

    dynamically add items to cart

--- labs/lab-5/assets/js/app.js
+++ labs/lab-5/assets/js/app.js
@@ -21,12 +21,25 @@ $('.item').click(function() {
             
         $('.item-count').html(new_count);
         $(this).addClass('in-cart');
+        //*add item to cart
+        add_to_cart(this);
     }
-    
+
     });
 
     $('.cart-toggle').click(function() {
         $('.cart').toggleClass('hide');
     }); 
 
+    function add_to_cart(line_item) {
+        // get img and price of item clicked 
+        // inject html with img and price 
+        var price = $(line_item).data('price'), 
+            img = $(line_item).data('img'), 
+            line_item_html = '<div class="line-item"><div class="line-item-img ' + img + '"></div><div class="line-item-price">' + price + '</div></div>';
+        
+        $('.line-items').append(line_item_html);
+        console.log('hi');
+    
+        }
   
