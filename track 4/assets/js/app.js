d5523291f0c2cb66fac10a752a1effeddb0655fe
Vidya Keshavan <vidyakeshavan91@gmail.com>
2018-03-05 19:19:14 -0500

    word play

---
+++ track 4/assets/js/app.js
@@ -0,0 +1,18 @@
+var animateText = document.getElementById("animated-text");
+var textArray = animateText.innerText.split("");
+
+animateText.firstChild.remove();
+
+var elArray = textArray.map(
+  (letter,index)=>{
+    if(letter==" ") letter = '&nbsp;';
+    var el = document.createElement("span");
+    el.className = "letter";
+    el.innerHTML = letter;
+    el.style.animationDelay = index/(textArray.length)+"s";
+    animateText.appendChild(el);
+    return el;
+  }
+);
+
+animateText.innerHtml = elArray;
